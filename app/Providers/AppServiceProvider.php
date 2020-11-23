ed066463be662dcc2be9b0283c1596953fc316cf
Tsunehito Higa <tsunehito@23prod.com>
2020-11-23 16:38:40 +0900

    add share variable 'quizzes' for sidebar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Quiz;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $quizzes = (new Quiz)->allQuiz();
+        View::share('quizzes', $quizzes);
     }
 }
